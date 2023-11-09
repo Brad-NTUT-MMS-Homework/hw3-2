e731d95fa604be6bdc6c44a97fa024159bfdd063
lrnbrad <lrnbrad@gmail.com>
2023-11-09 22:46:44 +0800

    Feat: Complete 5.36

---
+++ 5.36/source/main.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+void hanoi(int n, char init_rod, char end_rod, char aux_rod) {
+	if (n == 1) {
+		printf("Move disk 1 from %c to %c\n", init_rod, end_rod);
+		return;
+	}
+
+	// 先移到中間
+	hanoi(n - 1, init_rod, aux_rod, end_rod);
+	printf("Move disk %d from %c to %c\n", n, init_rod, end_rod);
+	// 再移到後面
+	hanoi(n - 1, aux_rod, end_rod, init_rod);
+}
+
+int main() {
+
+	int disks = 3;
+	hanoi(disks, 'A', 'C', 'B');
+}
